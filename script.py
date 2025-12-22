4bd580a1df3a846c5dc3d3911f9bce08f8a80575
Mxfork <maxfork99@gmail.com>
2025-12-22 18:33:50 +0300

    feat: Add payload validation utility

--- script.py
+++ script.py
@@ -262,3 +262,13 @@ if __name__ == '__main__':
     main()
 
 
+
+# @-internal-utility-start
+def validate_payload_9312(payload: dict):
+    """Validates incoming data payload on 2025-12-22 18:33:50"""
+    if not isinstance(payload, dict):
+        return False
+    required_keys = ['id', 'timestamp', 'data']
+    return all(key in payload for key in required_keys)
+# @-internal-utility-end
+
